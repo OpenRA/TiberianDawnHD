d209d13cb6596f48419712bb42a27dfad5f958c6
Paul Chote <pchote@users.noreply.github.com>
2023-01-03 12:02:50 +0000

    Implement masked animations for harv/proc/afld.

--- OpenRA.Mods.Mobius/RemasterSpriteSequence.cs
+++ OpenRA.Mods.Mobius/RemasterSpriteSequence.cs
@@ -13,6 +13,7 @@ using System.Collections.Generic;
 using System.Linq;
 using OpenRA.Graphics;
 using OpenRA.Mods.Common.Graphics;
+using OpenRA.Primitives;
 
 namespace OpenRA.Mods.Cnc.Graphics
 {
@@ -39,7 +40,7 @@ namespace OpenRA.Mods.Cnc.Graphics
 
 		public override ISpriteSequence CreateSequence(ModData modData, string tileset, SpriteCache cache, string image, string sequence, MiniYaml data, MiniYaml defaults)
 		{
-			return new RemasterSpriteSequence(modData, tileset, cache, this, image, sequence, data, defaults);
+			return new RemasterSpriteSequence(cache, this, image, sequence, data, defaults);
 		}
 	}
 
@@ -74,6 +75,9 @@ namespace OpenRA.Mods.Cnc.Graphics
 		[Desc("Dictionary of <tileset name>: <filename pattern> to override the RemasteredFilenamePattern key.")]
 		static readonly SpriteSequenceField<Dictionary<string, string>> RemasteredTilesetFilenamesPattern = new SpriteSequenceField<Dictionary<string, string>>(nameof(RemasteredTilesetFilenamesPattern), null);
 
+		[Desc("File name pattern of the sprite to mask the remastered sprite.")]
+		static readonly SpriteSequenceField<string> RemasteredMaskFilename = new SpriteSequenceField<string>(nameof(RemasteredMaskFilename), null);
+
 		[Desc("Change the position in-game on X, Y, Z.")]
 		protected static readonly SpriteSequenceField<float3> RemasteredOffset = new SpriteSequenceField<float3>(nameof(RemasteredOffset), float3.Zero);
 
@@ -81,8 +85,8 @@ namespace OpenRA.Mods.Cnc.Graphics
 
 		bool hasRemasteredSprite = true;
 
-		public RemasterSpriteSequence(ModData modData, string tileset, SpriteCache cache, ISpriteSequenceLoader loader, string image, string sequence, MiniYaml data, MiniYaml defaults)
-			: base(modData, tileset, cache, loader, image, sequence, data, defaults) { }
+		public RemasterSpriteSequence(SpriteCache cache, ISpriteSequenceLoader loader, string image, string sequence, MiniYaml data, MiniYaml defaults)
+			: base(cache, loader, image, sequence, data, defaults) { }
 
 		string ResolveTilesetId(string tileset, MiniYaml data, MiniYaml defaults)
 		{
@@ -120,7 +124,9 @@ namespace OpenRA.Mods.Cnc.Graphics
 			return spriteName;
 		}
 
-		protected override void ReserveSprites(ModData modData, string tileset, SpriteCache cache, MiniYaml data, MiniYaml defaults)
+		int? remasteredMaskToken;
+
+		public override void ReserveSprites(ModData modData, string tileset, SpriteCache cache, MiniYaml data, MiniYaml defaults)
 		{
 			var frames = LoadField(Frames, data, defaults);
 			var flipX = LoadField(FlipX, data, defaults);
@@ -128,7 +134,11 @@ namespace OpenRA.Mods.Cnc.Graphics
 			var zRamp = LoadField(ZRamp, data, defaults);
 			var offset = LoadField(Offset, data, defaults);
 			var remasteredOffset = LoadField(RemasteredOffset, data, defaults);
+			var remasteredMaskFilename = LoadField(RemasteredMaskFilename, data, defaults, out var remasteredMaskFilenameLocation);
 			var blendMode = LoadField(BlendMode, data, defaults);
+
+			if (!string.IsNullOrEmpty(remasteredMaskFilename))
+				remasteredMaskToken = cache.ReserveFrames(remasteredMaskFilename, null, remasteredMaskFilenameLocation);
 
 			var combineNode = data.Nodes.FirstOrDefault(n => n.Key == Combine.Key);
 			if (combineNode != null)
@@ -144,9 +154,15 @@ namespace OpenRA.Mods.Cnc.Graphics
 
 					foreach ((var subFilename, var subLoadFrames, var subUseFrames, var subLocation) in ParseRemasterCombineFilenames(tileset, subFrames, combineNode.Value.Nodes[i].Key, subData))
 					{
+						int token;
+						if (remasteredMaskToken != null)
+							token = cache.ReserveFrames(subFilename, subLoadFrames, subLocation);
+						else
+							token = cache.ReserveSprites(subFilename, subLoadFrames, subLocation);
+
 						spritesToLoad.Add(new SpriteReservation
 						{
-							Token = cache.ReserveSprites(subFilename, subLoadFrames, subLocation),
+							Token = token,
 							Offset = hasRemasteredSprite ? remasteredSubOffset + remasteredOffset : subOffset + offset,
 							FlipX = subFlipX ^ flipX,
 							FlipY = subFlipY ^ flipY,
@@ -161,9 +177,15 @@ namespace OpenRA.Mods.Cnc.Graphics
 			{
 				foreach ((var filename, var loadFrames, var useFrames, var location) in ParseRemasterFilenames(tileset, frames, data, defaults))
 				{
+					int token;
+					if (remasteredMaskToken != null)
+						token = cache.ReserveFrames(filename, loadFrames, location);
+					else
+						token = cache.ReserveSprites(filename, loadFrames, location);
+
 					spritesToLoad.Add(new SpriteReservation
 					{
-						Token = cache.ReserveSprites(filename, loadFrames, location),
+						Token = token,
 						Offset = hasRemasteredSprite ? remasteredOffset : offset,
 						FlipX = flipX,
 						FlipY = flipY,
@@ -173,6 +195,136 @@ namespace OpenRA.Mods.Cnc.Graphics
 					});
 				}
 			}
+		}
+
+		public override void ResolveSprites(SpriteCache cache)
+		{
+			if (bounds != null)
+				return;
+
+			Sprite depthSprite = null;
+			if (depthSpriteReservation != null)
+				depthSprite = cache.ResolveSprites(depthSpriteReservation.Value).First(s => s != null);
+
+			Sprite[] allSprites;
+			if (remasteredMaskToken != null)
+			{
+				var maskFrames = cache.ResolveFrames(remasteredMaskToken.Value);
+				var allFrames = spritesToLoad.SelectMany<SpriteReservation, (SpriteReservation, ISpriteFrame)>(r =>
+				{
+					var resolved = cache.ResolveFrames(r.Token);
+					if (r.Frames != null)
+						return r.Frames.Select(f => (r, resolved[f]));
+
+					return resolved.Select(f => (r, f));
+				}).ToArray();
+
+				var frameLength = length ?? allFrames.Length - start;
+				if (maskFrames.Length != frameLength)
+					throw new YamlException($"Sequence {image}.{Name} with {frameLength} frames cannot use mask with {maskFrames.Length} frames.");
+
+				allSprites = new Sprite[allFrames.Length];
+				for (var i = 0; i < frameLength; i++)
+				{
+					(var r, var frame) = allFrames[start + i];
+					var mask = maskFrames[i];
+					if (frame.Size != mask.Size)
+						throw new YamlException($"Sequence {image}.{Name} frame {i} with size {frame.Size} frames cannot use mask with size {mask.Size}.");
+
+					if (mask.Type != SpriteFrameType.Indexed8)
+						throw new YamlException($"Sequence {image}.{Name} mask frame {i} must be an indexed image.");
+
+					var data = new byte[frame.Data.Length];
+					var channels = frame.Data.Length / mask.Data.Length;
+					for (var j = 0; j < mask.Data.Length; j++)
+						if (mask.Data[j] != 0)
+							for (var k = 0; k < channels; k++)
+								data[j * channels + k] = frame.Data[j * channels + k];
+
+					var s = cache.SheetBuilders[SheetBuilder.FrameTypeToSheetType(frame.Type)]
+						.Add(data, frame.Type, frame.Size, 0, frame.Offset);
+
+					var dx = r.Offset.X + (r.FlipX ? -s.Offset.X : s.Offset.X);
+					var dy = r.Offset.Y + (r.FlipY ? -s.Offset.Y : s.Offset.Y);
+					var dz = r.Offset.Z + s.Offset.Z + r.ZRamp * dy;
+					s = new Sprite(s.Sheet, FlipRectangle(s.Bounds, r.FlipX, r.FlipY), r.ZRamp, new float3(dx, dy, dz), s.Channel, r.BlendMode);
+
+					if (depthSprite != null)
+					{
+						var cw = (depthSprite.Bounds.Left + depthSprite.Bounds.Right) / 2 + (int)(s.Offset.X + depthSpriteOffset.X);
+						var ch = (depthSprite.Bounds.Top + depthSprite.Bounds.Bottom) / 2 + (int)(s.Offset.Y + depthSpriteOffset.Y);
+						var w = s.Bounds.Width / 2;
+						var h = s.Bounds.Height / 2;
+
+						s = new SpriteWithSecondaryData(s, depthSprite.Sheet, Rectangle.FromLTRB(cw - w, ch - h, cw + w, ch + h), depthSprite.Channel);
+					}
+
+					allSprites[start + i] = s;
+				}
+			}
+			else
+			{
+				allSprites = spritesToLoad.SelectMany(r =>
+				{
+					var resolved = cache.ResolveSprites(r.Token);
+					if (r.Frames != null)
+						resolved = r.Frames.Select(f => resolved[f]).ToArray();
+
+					return resolved.Select(s =>
+					{
+						if (s == null)
+							return null;
+
+						var dx = r.Offset.X + (r.FlipX ? -s.Offset.X : s.Offset.X);
+						var dy = r.Offset.Y + (r.FlipY ? -s.Offset.Y : s.Offset.Y);
+						var dz = r.Offset.Z + s.Offset.Z + r.ZRamp * dy;
+						var sprite = new Sprite(s.Sheet, FlipRectangle(s.Bounds, r.FlipX, r.FlipY), r.ZRamp, new float3(dx, dy, dz), s.Channel, r.BlendMode);
+						if (depthSprite == null)
+							return sprite;
+
+						var cw = (depthSprite.Bounds.Left + depthSprite.Bounds.Right) / 2 + (int)(s.Offset.X + depthSpriteOffset.X);
+						var ch = (depthSprite.Bounds.Top + depthSprite.Bounds.Bottom) / 2 + (int)(s.Offset.Y + depthSpriteOffset.Y);
+						var w = s.Bounds.Width / 2;
+						var h = s.Bounds.Height / 2;
+
+						return new SpriteWithSecondaryData(sprite, depthSprite.Sheet, Rectangle.FromLTRB(cw - w, ch - h, cw + w, ch + h), depthSprite.Channel);
+					});
+				}).ToArray();
+			}
+
+			length = length ?? allSprites.Length - start;
+
+			if (alpha != null)
+			{
+				if (alpha.Length == 1)
+					alpha = Exts.MakeArray(length.Value, _ => alpha[0]);
+				else if (alpha.Length != length.Value)
+					throw new YamlException($"Sequence {image}.{Name} must define either 1 or {length.Value} Alpha values.");
+			}
+			else if (alphaFade)
+				alpha = Exts.MakeArray(length.Value, i => float2.Lerp(1f, 0f, i / (length.Value - 1f)));
+
+			// Reindex sprites to order facings anti-clockwise and remove unused frames
+			var index = CalculateFrameIndices(start, length.Value, stride ?? length.Value, facings, null, transpose, reverseFacings).ToList();
+			if (reverses)
+			{
+				index.AddRange(index.Skip(1).Take(length.Value - 2).Reverse());
+				length = 2 * length - 2;
+			}
+
+			if (!index.Any())
+				throw new YamlException($"Sequence {image}.{Name} does not define any frames.");
+
+			var minIndex = index.Min();
+			var maxIndex = index.Max();
+			if (minIndex < 0 || maxIndex >= allSprites.Length)
+				throw new YamlException($"Sequence {image}.{Name} uses frames between {minIndex}..{maxIndex}, but only 0..{allSprites.Length - 1} exist.");
+
+			sprites = index.Select(f => allSprites[f]).ToArray();
+			if (shadowStart >= 0)
+				shadowSprites = index.Select(f => allSprites[f - start + shadowStart]).ToArray();
+
+			bounds = sprites.Concat(shadowSprites ?? Enumerable.Empty<Sprite>()).Select(OffsetSpriteBounds).Union();
 		}
 
 		IEnumerable<(string Filename, int[] loadFrames, int[] useFrames, MiniYamlNode.SourceLocation location)> ParseRemasterFilenames(string tileset, int[] frames, MiniYaml data, MiniYaml defaults)
